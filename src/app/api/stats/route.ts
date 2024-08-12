41bc65bc12adeeb50a468809130ad997b01df8c3
shlhtch <shlhtch.dan@gmail.com>
2024-08-12 13:14:42 +0300

    add api post stats

--- src/app/api/stats/route.ts
+++ src/app/api/stats/route.ts
@@ -23,3 +23,80 @@ export async function GET() {
     );
   }
 }
+
+export async function POST(request: Request) {
+  try {
+    const {
+      date,
+      user_id,
+      deposits,
+      redeposits,
+      tir1,
+      tir2,
+      comment,
+    }: {
+      date: string;
+      user_id: number;
+      deposits: number;
+      redeposits: number;
+      tir1: number;
+      tir2: number | null;
+      comment: string;
+    } = await request.json();
+
+    const [day, month, year] = date.split("-");
+    const formattedDate = new Date(`${year}-${month}-${day}`);
+
+    if (
+      !date ||
+      user_id === undefined ||
+      deposits === undefined ||
+      redeposits === undefined ||
+      tir1 === undefined ||
+      (tir2 !== null && tir2 === undefined)
+    ) {
+      return NextResponse.json(
+        { error: "Некоторые поля имеют некорректные значения" },
+        { status: 400 }
+      );
+    }
+
+    const existingRecord = await prisma.stats.findUnique({
+      where: {
+        user_id_date: {
+          user_id,
+          date: formattedDate,
+        },
+      },
+    });
+
+    if (existingRecord) {
+      return NextResponse.json(
+        {
+          error: "Запись для для данной даты уже существует",
+        },
+        { status: 409 }
+      );
+    }
+
+    const newStat = await prisma.stats.create({
+      data: {
+        date: formattedDate,
+        user_id,
+        deposits,
+        redeposits,
+        tir1,
+        tir2,
+        comment,
+      },
+    });
+
+    return NextResponse.json(newStat, { status: 201 });
+  } catch (error) {
+    console.error("Ошибка при создании записи:", error);
+    return NextResponse.json(
+      { error: "Ошибка при создании записи" },
+      { status: 500 }
+    );
+  }
+}
