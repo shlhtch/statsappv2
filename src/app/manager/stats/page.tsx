72d49c1c849ed6da2eec66709704fd2a4bc7f8c0
shlhtch <shlhtch.dan@gmail.com>
2024-08-13 13:03:31 +0300

    ctrl+s

--- src/app/manager/stats/page.tsx
+++ src/app/manager/stats/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import StatsControl from '@/components/manager/StatsControl';
-import NavBar from '@/components/NavBar';
 
 export default function StatsPage() {
   return (
--- src/components/manager/StatsControl.tsx
+++ src/components/manager/StatsControl.tsx
@@ -2,19 +2,22 @@
 
 import { useEffect, useState, useCallback } from "react";
 
-
 const StatsControl = () => {
-  // Состояния для фильтров и данных
+  const getYesterdaysDate = () => {
+    const date = new Date();
+    date.setDate(date.getDate() - 1);
+    return date.toISOString().split("T")[0];
+  };
+
   const [teams, setTeams] = useState<ITeam[]>([]);
   const [filteredData, setFilteredData] = useState<IMember[]>([]);
   const [selectedTeam, setSelectedTeam] = useState<string>("");
   const [selectedMember, setSelectedMember] = useState<string>("");
-  const [selectedDate, setSelectedDate] = useState<string>("");
+  const [selectedDate, setSelectedDate] = useState<string>(getYesterdaysDate());
 
-  // Загрузка команд при монтировании компонента
   useEffect(() => {
     const fetchTeams = async () => {
-      const response = await fetch("/api/stats/dashboard"); // Используем новый эндпоинт
+      const response = await fetch("/api/stats/dashboard");
       const data: ITeam[] = await response.json();
       setTeams(data);
     };
@@ -22,9 +25,7 @@ const StatsControl = () => {
     fetchTeams();
   }, []);
 
-  // Общая функция изменения фильтров с использованием useCallback
   const handleFilterChange = useCallback(async () => {
-    // Фильтрация данных
     const filteredMembers = teams
       .flatMap((team) => team.members)
       .filter((member) => {
@@ -38,7 +39,7 @@ const StatsControl = () => {
           ? member.stats.some((stat: IStat) =>
               stat.date.startsWith(selectedDate)
             )
-          : true; // Уточнили тип
+          : true;
 
         return matchTeam && matchMember && matchDate;
       });
@@ -46,85 +47,90 @@ const StatsControl = () => {
     setFilteredData(filteredMembers);
   }, [teams, selectedTeam, selectedMember, selectedDate]);
 
-  // Фильтрация по выбранным значениям
   useEffect(() => {
     handleFilterChange();
   }, [handleFilterChange]);
 
   return (
     <div className="p-5">
-      <h2 className="text-lg font-bold mb-4">Статистика</h2>
-      <div className="mb-4">
-        <select
-          onChange={(e) => setSelectedTeam(e.target.value)}
-          value={selectedTeam}
-          className="p-2 border border-gray-300 rounded mr-2"
-        >
-          <option value="">Выберите команду</option>
-          {teams.map((team) => (
-            <option key={team.id} value={team.id}>
-              {team.title}
-            </option>
-          ))}
-        </select>
-
-        <select
-          onChange={(e) => setSelectedMember(e.target.value)}
-          value={selectedMember}
-          className="p-2 border border-gray-300 rounded mr-2"
-        >
-          <option value="">Выберите участника</option>
-          {teams.flatMap((team) =>
-            team.members.map((member) => (
-              <option key={member.id} value={member.id}>
-                {member.name}
-              </option>
-            ))
-          )}
-        </select>
-
-        <input
-          type="date"
-          onChange={(e) => setSelectedDate(e.target.value)}
-          value={selectedDate}
-          className="p-2 border border-gray-300 rounded"
-        />
-      </div>
-
-      <table className="min-w-full border border-gray-300">
-        <thead>
-          <tr className="bg-gray-200">
-            <th className="border p-2">Участник</th>
-            <th className="border p-2">Депы</th>
-            <th className="border p-2">Додепы</th>
-            <th className="border p-2">Tir1</th>
-            <th className="border p-2">Tir2</th>
-            <th className="border p-2">Комментарий</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.length > 0 ? (
-            filteredData.map((member) =>
-              member.stats.map((stat) => (
-                <tr key={stat.id}>
-                  <td className="border p-2">{member.name}</td>
-                  <td className="border p-2">{stat.deposits}</td>
-                  <td className="border p-2">{stat.redeposits}</td>
-                  <td className="border p-2">{stat.tir1}</td>
-                  <td className="border p-2">{stat.tir2}</td>
-                  <td className="border p-2">{stat.comment}</td>
-                </tr>
+      <div className="mb-4 p-4">
+        <div className="grid grid-cols-2 gap-4 mb-4">
+          <div>
+            <div>
+              <input
+                type="date"
+                onChange={(e) => setSelectedDate(e.target.value)}
+                value={selectedDate}
+                className="p-2 w-full bg-[#2F313B] rounded-xl"
+              />
+            </div>
+          </div>
+          <select
+            onChange={(e) => setSelectedMember(e.target.value)}
+            value={selectedMember}
+            className="p-2 w-ful bg-[#2F313B] rounded-xl"
+          >
+            <option value="">Выберите участника</option>
+            {teams.flatMap((team) =>
+              team.members.map((member) => (
+                <option key={member.id} value={member.id}>
+                  {member.name}
+                </option>
               ))
-            )
-          ) : (
-            <tr>
-              <td colSpan={6} className="border p-2 text-center">
-                Нет данных
-              </td>
+            )}
+          </select>
+        </div>
+        <div>
+          <select
+            onChange={(e) => setSelectedTeam(e.target.value)}
+            value={selectedTeam}
+            className="p-2 w-full bg-[#2F313B] rounded-xl"
+          >
+            <option value="">Все команды</option>
+            {teams.map((team) => (
+              <option key={team.id} value={team.id}>
+                {team.title}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div className="h-[320px] overflow-y-auto">
+        <table className="mt-4 min-w-full border border-gray-300">
+          <thead>
+            <tr className="bg-[#2F313B]">
+              <th className="border p-2">Участник</th>
+              <th className="border p-2">Депы</th>
+              <th className="border p-2">Додепы</th>
+              <th className="border p-2">Tir1</th>
+              <th className="border p-2">Tir2</th>
+              <th className="border p-2">Комментарий</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {filteredData.length > 0 ? (
+              filteredData.map((member) =>
+                member.stats.map((stat) => (
+                  <tr key={stat.id}>
+                    <td className="border p-2">{member.name}</td>
+                    <td className="border p-2">{stat.deposits}</td>
+                    <td className="border p-2">{stat.redeposits}</td>
+                    <td className="border p-2">{stat.tir1}</td>
+                    <td className="border p-2">{stat.tir2}</td>
+                    <td className="border p-2">{stat.comment}</td>
+                  </tr>
+                ))
+              )
+            ) : (
+              <tr>
+                <td colSpan={6} className="border p-2 text-center">
+                  Нет данных
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
