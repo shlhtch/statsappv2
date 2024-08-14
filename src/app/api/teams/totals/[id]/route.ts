36f625ca2cbda6e3257b58a97f31521a4cf0e964
shlhtch <shlhtch.dan@gmail.com>
2024-08-14 16:35:36 +0300

    add team totals

--- src/app/api/teams/totals/[id]/route.ts
+++ src/app/api/teams/totals/[id]/route.ts
@@ -8,7 +8,6 @@ export async function GET(
   { params }: { params: { id: string } }
 ) {
   const { id } = params;
-
   try {
     const user = await prisma.users.findUnique({
       where: { telegramId: id },
@@ -31,19 +30,19 @@ export async function GET(
 
     const team = await prisma.teams.findUnique({
       where: { id: user.team_id },
-	  include: {
-		members: {
-			select: {
-				name: true,
-				stats: {
-					select: {
-						date: true,
-						totals: true
-					}
-				}
-			}
-		}
-	  }
+      include: {
+        members: {
+          select: {
+            name: true,
+            stats: {
+              select: {
+                date: true,
+                totals: true,
+              },
+            },
+          },
+        },
+      },
     });
 
     if (!team) {
@@ -52,6 +51,21 @@ export async function GET(
         { status: 404 }
       );
     }
+
+    // Получаем дату вчерашнего дня
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    yesterday.setHours(0, 0, 0, 0); // Устанавливаем время на начало дня
+    const endOfYesterday = new Date(yesterday);
+    endOfYesterday.setHours(23, 59, 59, 999); // Устанавливаем время на конец дня
+
+    // Фильтруем статистику для каждого участника
+    team.members.forEach((member) => {
+      member.stats = member.stats.filter((stat) => {
+        const statDate = new Date(stat.date);
+        return statDate >= yesterday && statDate <= endOfYesterday;
+      });
+    });
 
     return NextResponse.json({ user, team });
   } catch (error) {
